db5941af712a3901f42bac8c2ae300857b18974a
Sergei S <div@altertech.com>
2021-09-10 03:31:54 +0200

    example fix

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This is a helper library to create custom pools of anything
 
 ## Crate
 
-https://crates.io/crates/simple-pool
+<https://crates.io/crates/simple-pool>
 
 ## Example
 
@@ -22,22 +22,21 @@ async fn test() {
 	// create local or static resource pool
     let resource_pool: Arc<RwLock<ResourcePool<TcpStream>>> =
         Arc::new(RwLock::new(ResourcePool::new()));
-    let mut pool = resource_pool.write().await;
-	// put 20 tcp connections there
-    for _ in 0..20 {
-        let client = TcpStream::connect("127.0.0.1:80").await.unwrap();
-        pool.append(client);
+    {
+        let mut pool = resource_pool.write().await;
+        // put 20 tcp connections there
+        for _ in 0..20 {
+            let client = TcpStream::connect("127.0.0.1:80").await.unwrap();
+            pool.append(client);
+        }
     }
-    drop(pool);
-    let mut fut = Vec::new();
-    let started = std::time::Instant::now();
     let n = 1_000_000;
     for _ in 0..n {
         let res_pool = resource_pool.clone();
-        fut.push(tokio::spawn(async move {
+        tokio::spawn(async move {
             // gets open tcp connection as soon as one is available
             let _client = res_pool.read().await.get().await;
-        }));
+        });
     }
 }
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,11 @@
 //! 
 //! ## The idea
 //! 
-//! This is a helper library to create custom pools of anything.
+//! This is a helper library to create custom pools of anything
+//! 
+//! ## Crate
+//! 
+//! <https://crates.io/crates/simple-pool>
 //! 
 //! ## Example
 //! 
@@ -18,22 +22,21 @@
 //! 	// create local or static resource pool
 //!     let resource_pool: Arc<RwLock<ResourcePool<TcpStream>>> =
 //!         Arc::new(RwLock::new(ResourcePool::new()));
-//!     let mut pool = resource_pool.write().await;
-//! 	// put 20 tcp connections there
-//!     for _ in 0..20 {
-//!         let client = TcpStream::connect("127.0.0.1:80").await.unwrap();
-//!         pool.append(client);
+//!     {
+//!         let mut pool = resource_pool.write().await;
+//!         // put 20 tcp connections there
+//!         for _ in 0..20 {
+//!             let client = TcpStream::connect("127.0.0.1:80").await.unwrap();
+//!             pool.append(client);
+//!         }
 //!     }
-//!     drop(pool);
-//!     let mut fut = Vec::new();
-//!     let started = std::time::Instant::now();
 //!     let n = 1_000_000;
 //!     for _ in 0..n {
 //!         let res_pool = resource_pool.clone();
-//!         fut.push(tokio::spawn(async move {
-//!             // gets one of 20 open tcp connections as soon as one is available
+//!         tokio::spawn(async move {
+//!             // gets open tcp connection as soon as one is available
 //!             let _client = res_pool.read().await.get().await;
-//!         }));
+//!         });
 //!     }
 //! }
 //! ```
